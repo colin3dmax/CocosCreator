62394f9ee729fa1f6b203b9a06fbcbe2922af031
colin3dmax <colin3dmax@163.com>
2016-05-10 17:07:58 +0800

    修复Effect模拟器下报错Bug

--- Shader/library/imports/22/22b86430-0def-402c-b3c4-305cc4afbfc8.js
+++ Shader/library/imports/22/22b86430-0def-402c-b3c4-305cc4afbfc8.js
@@ -1,3 +1,3 @@
 /* 底片镜像 */
 
-module.exports = "precision mediump float;\n" + "varying vec2 v_texCoord;\n" + "void main()\n" + "{\n" + "	gl_FragColor = vec4(1.0 - texture2D(CC_Texture0, v_texCoord).rgb, 1.0);\n" + "}\n";
+module.exports = "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n" + "varying vec2 v_texCoord;\n" + "void main()\n" + "{\n" + "	gl_FragColor = vec4(1.0 - texture2D(CC_Texture0, v_texCoord).rgb, 1.0);\n" + "}\n";
--- Shader/library/imports/e6/e6a512c6-6086-440b-b57a-d63e4a8a0987.js
+++ Shader/library/imports/e6/e6a512c6-6086-440b-b57a-d63e4a8a0987.js
@@ -1,3 +1,3 @@
 /* 水平波浪 */
 
-module.exports = "precision mediump float;\n" + "varying vec2 v_texCoord;\n" + "uniform float motion;\n" + "uniform float angle;\n" + "void main()\n" + "{\n" + "    vec2 tmp = v_texCoord;\n" + "    tmp.x = tmp.x + 0.05 * sin(motion +  tmp.y * angle);\n" + "    gl_FragColor = texture2D(CC_Texture0, tmp);\n" + "}\n";
+module.exports = "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n" + "varying vec2 v_texCoord;\n" + "uniform float motion;\n" + "uniform float angle;\n" + "void main()\n" + "{\n" + "    vec2 tmp = v_texCoord;\n" + "    tmp.x = tmp.x + 0.05 * sin(motion +  tmp.y * angle);\n" + "    gl_FragColor = texture2D(CC_Texture0, tmp);\n" + "}\n";
